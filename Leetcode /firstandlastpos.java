2e6106f9fbf1e84d06098e4589bab34690b09de2
NAETHRAMARIMUTHU <naethra1111@gmail.com>
2026-09-25 20:08:37 +0530

    Create firstandlastpos.java

---
+++ Leetcode /firstandlastpos.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int left=-1;
+        int right=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                left=i;
+                break;
+            }
+        }
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i]==target){
+                right=i;
+                break;
+        }
+
+
+        }
+        return new int[]{left, right};
+    }
+}
